3609d8ba36aa76cb2b2617da3f7ceeb9eb920a8d
Thomas Gatzweiler <thomas.gatzweiler@gmail.com>
2010-08-02 17:26:33 +0200

    some small version fixes

--- include/version.hh
+++ include/version.hh
@@ -4,7 +4,4 @@
 #define PYRITE_VERSION    "0.1"
 #define PYRITE_COPYRIGHT  "Copyright © 2009-2010 Thomas Gatzweiler"
 
-#define COMPILE_DATE      __DATE__
-#define COMPILE_TIME      __TIME__
-
 #endif
