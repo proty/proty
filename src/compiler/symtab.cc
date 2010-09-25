4e9c26e6769339e7b5d28d7b763b7eca940b68a8
Thomas Gatzweiler <thomas.gatzweiler@gmail.com>
2010-09-25 22:18:45 +0200

    new symbol table

---
+++ src/compiler/symtab.cc
@@ -0,0 +1,33 @@
+#include "compiler/symtab.hh"
+
+namespace pyrite {
+
+  void Scope::set(std::string name, Value* value) {
+    variables[name] = value;
+  }
+
+  Value* Scope::get(std::string name) {
+    return variables[name];
+  }
+
+  void SymbolTable::enterScope() {
+    scopes.push_back(new Scope());
+  }
+
+  void SymbolTable::leaveScope() {
+    scopes.pop_back();
+  }
+
+  Value* SymbolTable::lookup(std::string name) {
+    Value* v;
+    for (int i = scopes.size()-1; i >= 0; i--) {
+      if ((v = scopes[i]->get(name))) return v;
+    }
+    return v;
+  }
+
+  void SymbolTable::store(std::string name, Value* value) {
+    scopes.back()->set(name, value);
+  }
+
+}
