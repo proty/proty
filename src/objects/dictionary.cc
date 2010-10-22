0346695e258fbd78fa157034de1ecf00cde0f4f9
Thomas Gatzweiler <thomas.gatzweiler@gmail.com>
2010-10-22 21:31:49 +0200

    dictionary now working

--- src/objects/dictionary.cc
+++ src/objects/dictionary.cc
@@ -31,18 +31,29 @@ namespace pyrite {
 
   Object* Dictionary::get(Object* key) {
     unsigned int h = hash(key) % bounds;
-    std::cout << "searched: " << key << std::endl;
-    std::cout << "hash: " << h << " key: " << content[h]->key << std::endl;
-    while ((content[h] == 0) || (content[h]->key != key)) {
-      h = ((h+1) % bounds);
-      std::cout << "hash: " << h << std::endl;
-      if (content[h]) std::cout << "key: " << content[h]->key << ((content[h] == 0) || (content[h]->key != key)) << std::endl;
-    }
-    std::cout << "hash: " << h << " key: " << content[h]->key << std::endl;
+    std::cout << h << std::endl;
+    while ((content[h] == 0) || (content[h]->key != key)) h = ((h+1) % bounds);
     return content[h]->value;
   }
 
   Object* Dictionary::set(Object* key, Object* value) {
+    if (float(size)/float(bounds) > 0.75) {
+      Bucket** tmp = new Bucket*[bounds*2];
+      memset(tmp, 0, bounds*2*sizeof(Bucket*));
+
+      for (int i = 0; i < bounds; i++) {
+        if (content[i] != 0) {
+          unsigned int h = hash(content[i]->key) %  bounds * 2;
+          while (content[h] != 0 && content[h]->key != key) h = ((h+1) % bounds);
+          tmp[h] = content[i];
+        }
+      }
+
+      delete [] content;
+      content = tmp;
+      bounds *= 2;
+    }
+
     unsigned int h = hash(key) % bounds;
 
     Bucket* b = new Bucket;
@@ -54,51 +65,7 @@ namespace pyrite {
     content[h] = b;
     size++;
 
-    if (float(size)/float(bounds) > 0.75) {
-      int oldbounds = bounds;
-      bounds *= 2;
-
-      Bucket** tmp = new Bucket*[bounds];
-      memset(tmp, 0, bounds*sizeof(Bucket*));
-
-      for (int i = 0; i < oldbounds; i++) {
-        if (content[i] != 0) {
-          unsigned int h = hash(content[i]->key) %  bounds;
-          tmp[h] = content[i];
-        }
-      }
-
-      delete content;
-      content = tmp;
-    }
-
     return 0;
   }
 
-}
-
-int main() {
-  pyrite::Dictionary* dict = new pyrite::Dictionary();
-  
-  pyrite::String* test1 = new pyrite::String("Hello");
-  pyrite::String* test2 = new pyrite::String("World");
-  pyrite::String* test3 = new pyrite::String("Foo");
-  pyrite::String* test4 = new pyrite::String("Bar");
-  pyrite::String* test5 = new pyrite::String("Test");
-  pyrite::String* test6 = new pyrite::String("This");
-  pyrite::String* test7 = new pyrite::String("Fucking");
-  pyrite::String* test8 = new pyrite::String("Code");
-  pyrite::String* test9 = new pyrite::String("Now");
- 
-  std::cout << dict->set(test1, test2) << std::endl;
-  std::cout << dict->set(test2, test1) << std::endl;
-  std::cout << dict->set(test3, test1) << std::endl;
-  std::cout << dict->set(test4, test1) << std::endl;
-  std::cout << dict->set(test5, test1) << std::endl;
-  std::cout << dict->set(test6, test1) << std::endl;
-  std::cout << dict->set(test7, test1) << std::endl;
-  std::cout << dict->set(test8, test1) << std::endl;
-  std::cout << dict->set(test9, test1) << std::endl;
-
-  std::cout << ((pyrite::String*)dict->get(test1))->get_value() << std::endl;
 }
