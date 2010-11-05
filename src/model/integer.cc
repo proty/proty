34653f8aa219f0ff1d3fb271986567cfc2831ec7
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2010-11-05 23:04:21 +0100

    bool and integer codegen + newint and newbool instructions

--- src/model/integer.cc
+++ src/model/integer.cc
@@ -1,13 +1,14 @@
 #include "model/models.hh"
 
 namespace pyrite {
-  
+
   Value* IntegerModel::codegen(Compiler* c) {
-    Value* integer = ConstantInt::get(Type::getInt32Ty(getGlobalContext()), value);
-    CallArgsModel* args = new CallArgsModel();
-    args->push(new ValueModel(integer));
-    CallModel* call = new CallModel("Integer::new", args);
-    return call->codegen(c);
+    Function* F = c->module->getFunction("newint");
+
+    std::vector<Value*> args;
+    args.push_back(ConstantInt::get(Type::getInt32Ty(getGlobalContext()), value));
+
+    return c->builder->CreateCall(F, args.begin(), args.end(), "inttmp");
   }
 
 }
