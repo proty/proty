d19fcdb43fc57df08abac7720481ecd914425a85
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2010-11-06 17:17:36 +0100

    binary operators are now working (only with integers)

--- src/objects/integer.cc
+++ src/objects/integer.cc
@@ -2,4 +2,24 @@
 
 namespace pyrite {
 
+  Object* Integer::operator+(Object* b) {
+    return new Integer(this->value + (int)*(Integer*)b);
+  }
+
+  Object* Integer::operator-(Object* b) {
+    return new Integer(this->value - (int)*(Integer*)b);
+  }
+
+  Object* Integer::operator*(Object* b) {
+    return new Integer(this->value * (int)*(Integer*)b);
+  }
+
+  Object* Integer::operator/(Object* b) {
+    return new Integer(this->value / (int)*(Integer*)b);
+  }
+
+  Object* Integer::operator%(Object* b) {
+    return new Integer(this->value % (int)*(Integer*)b);
+  }
+
 }
