0b15cba5698e141832a902b5cc8eb6120a57b366
Thomas Gatzweiler <thomas.gatzweiler@gmail.com>
2010-11-29 21:12:40 +0100

    fixes

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -1,13 +1,18 @@
 import builder
 
 class Compiler(object):
-  def __init__(self):
-    self.builder = builder.Builder()
+    def __init__(self):
+        self.builder = builder.Builder()
 
-  def compile(self):
-    self.builder.instr("add", ["new Integer(1)", "new Integer(2)"])
-    self.builder.instr("print", ["new Integer(1)"])
-    print(self.builder.write())
+    def compile(self, ast):
+        self.builder.instr("add", ["new Integer(1)", "new Integer(2)"])
+        self.builder.instr("print", ["new Integer(1)"])
+        print(self.builder.write())
+
+ast = [
+  "+", [5, 5],
+  "call", ["print", 5],
+]
 
 c = Compiler()
-c.compile()
+c.compile(ast)
