aedd2b4773aad3ef44e97095aa2710b806c9f756
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2011-01-05 16:06:01 +0100

    some simple fixes

--- compiler/ast.cc
+++ compiler/ast.cc
@@ -52,5 +52,16 @@ Value* IntegerNode::codegen(Compiler* c) {
 }
 
 Value* StringNode::codegen(Compiler* c) {
-  return 0;
+  size_t pos = 0;
+  while((pos = value.find("\\n", pos)) != std::string::npos) {
+    value.replace(pos, 2, "\n");
+    pos += 1;
+  }
+
+  Value* string = c->builder->CreateGlobalStringPtr(value.c_str(), ".str");
+
+  Function* func = c->module->getFunction("newstring");
+  std::vector<Value*> args;
+  args.push_back(string);
+  return c->builder->CreateCall(func, args.begin(), args.end(), "stringtmp");
 }
