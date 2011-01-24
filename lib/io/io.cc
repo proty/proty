1789a079835450f142ad323a3106f917b95ddc77
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2011-01-24 21:01:14 +0100

    adds io library and cmake file to build libraries

---
+++ lib/io/io.cc
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <objects.hh>
+
+using namespace proty;
+
+Object* io_print(Object* obj) {
+  printf("%s", (const char*)obj);
+  return Bool::False;
+}
+
+Object* io_init() {
+  Object* io = new Object();
+
+  Function* write = new Function((FunctionPtr*)io_print, 1);
+  io->setProperty(new String("write"), write);
+
+  return io;
+}
