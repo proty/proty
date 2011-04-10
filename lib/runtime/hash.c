c556eb2e21c5d3336458700bbcf18200175339a1
Thomas Gatzweiler <thomas.gatzweiler@gmail.com>
2011-04-10 18:44:19 +0200

    starts hash implementation

--- lib/runtime/hash.c
+++ lib/runtime/hash.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "runtime.h"
 
 Object* Hash_createProto() {
@@ -10,13 +11,30 @@ Object* Hash_createProto() {
 }
 
 Object* Hash_init(Object* self) {
+  Hash* hash = malloc(sizeof(Hash));
 
+  hash->size = 0;
+  hash->bounds = 8;
+
+  hash->keys = malloc(sizeof(Symbol*)*hash->bounds);
+  hash->values = malloc(sizeof(Object*)*hash->bounds);
+
+  self->data.ptr = hash;
+  return self;
 }
 
 Object* Hash_set(Object* self, Object* key, Object* value) {
+  Hash* hash = self->data.ptr;
 
+  if ((float)hash->size/hash->bounds > 0.75) {
+    hash->bounds *= 2;
+    hash->keys = realloc(hash->keys, hash->bounds*sizeof(Symbol*));
+    hash->values = realloc(hash->values, hash->bounds*sizeof(Object*));
+  }
+
+  return 0;
 }
 
 Object* Hash_get(Object* self, Object* key) {
-
+  return 0;
 }
