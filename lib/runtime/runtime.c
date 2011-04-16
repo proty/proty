da166028e54e25e9f4f5bf109d8c96ec0b9efdc5
Thomas Gatzweiler <thomas.gatzweiler@gmail.com>
2011-04-16 19:00:24 +0200

    adds String object

--- lib/runtime/runtime.c
+++ lib/runtime/runtime.c
@@ -4,10 +4,14 @@ Object* Object_proto;
 Object* Symbol_proto;
 Object* Function_proto;
 Object* Hash_proto;
+Object* Integer_proto;
+Object* String_proto;
 
 void runtime_init() {
   Object_proto = Object_createProto();
   Symbol_proto = Symbol_createProto();
   Function_proto = Function_createProto();
   Hash_proto = Hash_createProto();
+  Integer_proto = Integer_createProto();
+  String_proto = String_createProto();
 }
---
+++ lib/runtime/string.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include "runtime.h"
+
+Object* String_createProto() {
+  Object* proto = Object_new(Object_proto);
+
+  Object_setSlot(proto, Symbol_get("+"), Function_new((FuncPtr)String_add));
+
+  return proto;
+}
+
+Object* String_new(const char* value) {
+  Object* new = Object_new(String_proto);
+  char* v = malloc(sizeof(char)*strlen(value));
+  strcpy(v, value);
+  new->data.ptr = v;
+  return new;
+}
+
+Object* String_add(Object* self, Object* other) {
+  return self;
+}
