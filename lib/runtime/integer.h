04a162824fb2a9ef613783f2aa84000a9b55731c
Thomas Gatzweiler <thomas.gatzweiler@gmail.com>
2011-07-03 20:09:14 +0200

    cleans up integer.h

--- lib/runtime/integer.h
+++ lib/runtime/integer.h
@@ -6,19 +6,4 @@ Object* Integer_createProto();
 
 Object* Integer_new(int);
 
-Object* Integer_str(Object*);
-Object* Integer_bool(Object*);
-
-Object* Integer_add(Object*, Object*);
-Object* Integer_sub(Object*, Object*);
-Object* Integer_mul(Object*, Object*);
-Object* Integer_div(Object*, Object*);
-
-Object* Integer_eq(Object*, Object*);
-Object* Integer_ne(Object*, Object*);
-Object* Integer_lt(Object*, Object*);
-Object* Integer_gt(Object*, Object*);
-Object* Integer_le(Object*, Object*);
-Object* Integer_ge(Object*, Object*);
-
 #endif
