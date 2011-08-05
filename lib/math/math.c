1cf323ee51605936d808a12ff7d3a2ac1b86ba07
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2011-08-05 20:27:28 +0200

    math library fix

--- lib/math/math.c
+++ lib/math/math.c
@@ -1,59 +1,58 @@
 #include "runtime/runtime.h"
 #include <math.h>
 
-Object* math_sin(Object* self, Object* num) {
+MODFUNC(math_sin, Object* self, Object* num) {
   return Float_new(sin(num->data.d));
 }
 
-Object* math_cos(Object* self, Object* num) {
+MODFUNC(math_cos, Object* self, Object* num) {
   return Float_new(cos(num->data.d));
 }
 
-Object* math_tan(Object* self, Object* num) {
+MODFUNC(math_tan, Object* self, Object* num) {
   return Float_new(tan(num->data.d));
 }
 
-Object* math_asin(Object* self, Object* num) {
+MODFUNC(math_asin, Object* self, Object* num) {
   return Float_new(asin(num->data.d));
 }
 
-Object* math_acos(Object* self, Object* num) {
+MODFUNC(math_acos, Object* self, Object* num) {
   return Float_new(acos(num->data.d));
 }
 
-Object* math_atan(Object* self, Object* num) {
+MODFUNC(math_atan, Object* self, Object* num) {
   return Float_new(atan(num->data.d));
 }
 
-Object* math_log(Object* self, Object* num) {
+MODFUNC(math_log, Object* self, Object* num) {
   return Float_new(log(num->data.d));
 }
 
-Object* math_pow(Object* self, Object* base, Object* exponent) {
+MODFUNC(math_pow, Object* self, Object* base, Object* exponent) {
   return Float_new(pow(base->data.d, exponent->data.d));
 }
 
-Object* math_sqrt(Object* self, Object* num) {
+MODFUNC(math_sqrt, Object* self, Object* num) {
   return Float_new(sqrt(num->data.d));
 }
 
-Object* math_init() {
-  Object* math = Object_new(Object_proto);
+EXPORT(math_pi);
+EXPORT(math_e);
 
-  Object_setSlot(math, "sin", FUNC(math_sin, 2));
-  Object_setSlot(math, "cos", FUNC(math_cos, 2));
-  Object_setSlot(math, "tan", FUNC(math_cos, 2));
-  Object_setSlot(math, "asin", FUNC(math_asin, 2));
-  Object_setSlot(math, "acos", FUNC(math_acos, 2));
-  Object_setSlot(math, "atan", FUNC(math_atan, 2));
+void math_init() {
+  MODINIT(math_sin, FUNC(math_sin, 2));
+  MODINIT(math_cos, FUNC(math_cos, 2));
+  MODINIT(math_tan, FUNC(math_cos, 2));
+  MODINIT(math_asin, FUNC(math_asin, 2));
+  MODINIT(math_acos, FUNC(math_acos, 2));
+  MODINIT(math_atan, FUNC(math_atan, 2));
 
-  Object_setSlot(math, "log", FUNC(math_log, 2));
+  MODINIT(math_log, FUNC(math_log, 2));
 
-  Object_setSlot(math, "pow", FUNC(math_pow, 3));
-  Object_setSlot(math, "sqrt", FUNC(math_sqrt, 2));
+  MODINIT(math_pow, FUNC(math_pow, 3));
+  MODINIT(math_sqrt, FUNC(math_sqrt, 2));
 
-  Object_setSlot(math, "pi", Float_new(3.141592));
-  Object_setSlot(math, "e", Float_new(2.718281));
-
-  return math;
+  MODINIT(math_pi, Float_new(3.141592));
+  MODINIT(math_e, Float_new(2.718281));
 }
