5b2a0cb074bda1111fd8b6bbab4f7eae0d416d82
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2012-01-21 20:00:56 +0100

    adds string method declarations

--- lib/runtime/string.h
+++ lib/runtime/string.h
@@ -5,5 +5,12 @@ extern Object* String_proto;
 void String_initProto();
 
 Object* String_new(const char*);
+Object* String_add(Object* self, Object* other);
+Object* String_iadd(Object* self, Object* other);
+Object* String_eq(Object* self, Object* other);
+Object* String_ne(Object* self, Object* other);
+Object* String_bool(Object* self);
+Object* String_split(Object* self, Object* delimiter);
+Object* String_length(Object* self);
 
 #endif
