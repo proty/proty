ccf8ab7983a434cf71265e2c6180da14e0088367
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2012-04-01 14:16:35 +0200

    provide environment information

---
+++ src/env.c
@@ -0,0 +1,13 @@
+#include "env.h"
+
+const char* proty_version() {
+    return VERSION;
+}
+
+const char* proty_platform() {
+    return PLATFORM;
+}
+
+const char* proty_prefix() {
+    return PREFIX;
+}
