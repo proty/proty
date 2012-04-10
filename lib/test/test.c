3928407b74879924e1c3e0d7678ef0b103f93710
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2012-04-10 14:59:01 +0200

    new test script

--- lib/test/test.c
+++ lib/test/test.c
@@ -1,42 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "runtime/runtime.h"
+#include "io/io.h"
 
 Object* test_context;
 
 Object* test_run(Object* self, Object* name, Object* function) {
-    Object_call(function, 1, &test_context);
-    return Qnil;
-}
+    printf("testing: %s\n", (char*)name->data.ptr);
+    test_context->data.i = 1;
 
-Object* test_assert(Object* self, Object* value) {
-    if (value == Qtrue) printf("+");
-    else printf("-");
+    Object_call(function, 1, &test_context);
+
+    if (test_context->data.i) printf("status: passed");
+    else printf("status: failed");
+
+    printf("\n---\n");
+
     return Qnil;
 }
 
 Object* test_assertEqual(Object* self, Object* a, Object* b) {
     Object* value = Object_send(a, SYM(==), 1, &b);
-    if (value == Qtrue) printf("+");
-    else printf("-");
+    if (value != Qtrue) {
+        printf("assertion failed: ");
+        io_write(0, a);
+        printf(" != ");
+        io_write(0, b);
+        printf("\n");
+
+        self->data.i = 0;
+    }
     return Qnil;
 }
 
 Object* test_assertTrue(Object* self, Object* obj) {
-    if (obj == Qtrue) printf("+");
-    else printf("-");
+    Object* boolean = Object_send(obj, SYM(bool), 0, 0);
+
+    if (boolean != Qtrue) {
+        printf("assertion failed: ");
+        io_write(0, obj);
+        printf(" is not true\n");
+
+        self->data.i = 0;
+    }
     return Qnil;
 }
 
 Object* test_assertFalse(Object* self, Object* obj) {
-    if (obj == Qfalse) printf("+");
-    else printf("-");
+    Object* boolean = Object_send(obj, SYM(bool), 0, 0);
+
+    if (boolean != Qfalse) {
+        printf("assertion failed: ");
+        io_write(0, obj);
+        printf(" is not false\n");
+
+        self->data.i = 0;
+    }
     return Qnil;
 }
 
 Object* test_init() {
     test_context = Object_new(Object_proto);
-    Object_setSlot(test_context, SYM(assert), FUNC(test_assert, 2));
     Object_setSlot(test_context, SYM(assertEqual), FUNC(test_assertEqual, 3));
     Object_setSlot(test_context, SYM(assertTrue), FUNC(test_assertTrue, 2));
     Object_setSlot(test_context, SYM(assertFalse), FUNC(test_assertFalse, 2));
