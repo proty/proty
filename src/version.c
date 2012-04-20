f45ca2149ba35294aea33784e561a1b14e59eb0a
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2012-04-20 14:26:40 +0200

    updates version.c

---
+++ src/version.c
@@ -0,0 +1,13 @@
+#include "version.h"
+
+const int proty_version[3] = {
+  VERSION_MAJOR,
+  VERSION_MINOR,
+  VERSION_PATCH
+};
+
+const char* proty_version_string = VERSION;
+const char* proty_platform = PLATFORM;
+const char* proty_prefix = PREFIX;
+const char* proty_build_type = BUILD_TYPE;
+const char* proty_copyright = COPYRIGHT;
---
+++ src/version.h
@@ -0,0 +1,11 @@
+#ifndef PROTY_VERSION_H
+#define PROTY_VERSION_H
+
+extern const int proty_version[3];
+extern const char* proty_version_string;
+extern const char* proty_platform;
+extern const char* proty_prefix;
+extern const char* proty_build_type;
+extern const char* proty_copyright;
+
+#endif
